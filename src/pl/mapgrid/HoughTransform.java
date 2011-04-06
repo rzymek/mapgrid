a6e5ad8642024d633c2eab662c4cb96f9a2b21d3
rzymek <rzymek@bc6c1078-3195-aa7d-df54-58ac2b8c4f42>
2011-04-06 15:32:21 +0000

    t,r -> angle,dist
    git-svn-id: http://mapgrid.googlecode.com/svn/trunk@3 bc6c1078-3195-aa7d-df54-58ac2b8c4f42

--- src/pl/mapgrid/HoughTransform.java
+++ src/pl/mapgrid/HoughTransform.java
@@ -12,50 +12,50 @@ import javax.imageio.ImageIO;
 public class HoughTransform {
 	private static final double THRESHOLD = 0.3;
 	private static final short MAX_SPACE_VALUE = Short.MAX_VALUE;
-	private static final int R_MIN = 5;
+	private static final int DIST_MIN = 5;
 
 	public BufferedImage tranform2(BufferedImage src) {
 		long start = System.currentTimeMillis();
 		coord.clear();
 		
 		double aberration = Math.toRadians(10);
-		double tstep=Math.toRadians(0.05);
+		double angleStep=Math.toRadians(0.05);
 		
-		int twidth = (int) (2*aberration/tstep);
-		double[] tv_0 = new double[twidth];
-		double[] tv_90 = new double[twidth];
-		double[] sin = new double[twidth];
-		double[] cos = new double[twidth];
+		int angleSpace = (int) (2*aberration/angleStep);
+		double[] angle_0 = new double[angleSpace];
+		double[] angle_90 = new double[angleSpace];
+		double[] sin = new double[angleSpace];
+		double[] cos = new double[angleSpace];
 		
-		double tmin_0 = -aberration; 
+		double angleMin_0 = -aberration; 
 //		double tmax_0 = +aberration;
 
-		double tmin_90 = Math.PI/2-aberration; 
+		double angleMin_90 = Math.PI/2-aberration; 
 //		double tmax_90 = Math.PI/2+aberration;
 		
-		for (int i = 0; i < twidth; i++) {
-			tv_0[i] = tmin_0+tstep*i;
-			tv_90[i] = tmin_90+tstep*i;
-			sin[i] = Math.sin(tv_0[i]);
-			cos[i] = Math.cos(tv_0[i]);
+		for (int i = 0; i < angleSpace; i++) {
+			angle_0[i] = angleMin_0+angleStep*i;
+			angle_90[i] = angleMin_90+angleStep*i;
+			sin[i] = Math.sin(angle_0[i]);
+			cos[i] = Math.cos(angle_0[i]);
 			//sin_90[i] = cos[i]
 			//cos_90[i] = -sin[i]
 		}
 		int w = src.getWidth();
 		int h = src.getHeight();
-		int rmax=Math.max(w,h);
-		short[][] space_0 = new short[twidth+1][rmax+1];
-		short[][] space_90 = new short[twidth+1][rmax+1];
+		int distMax=Math.max(w,h);
+		short[][] space_0 = new short[angleSpace+1][distMax+1];
+		short[][] space_90 = new short[angleSpace+1][distMax+1];
 		for(int x=0;x<w;++x) {
 			if(x % 100 ==0)
 				System.out.println( 100*x/w);
 			for (int y = 0; y < h; ++y) {
 				if (isPoint(src, x, y)) {
-					for (int i = 0; i < twidth; ++i) {
-						int r_0 =  (int) (y * sin[i] + x * cos[i]);
-						int r_90 = (int) (y * cos[i] - x * sin[i]);
-						incrementSpace(rmax, i, r_0, space_0);
-						incrementSpace(rmax, i, r_90, space_90);
+					for (int i = 0; i < angleSpace; ++i) {
+						int dist_0 =  (int) (y * sin[i] + x * cos[i]);
+						int dist_90 = (int) (y * cos[i] - x * sin[i]);
+						incrementSpace(distMax, i, dist_0, space_0);
+						incrementSpace(distMax, i, dist_90, space_90);
 					}
 				}
 			}
@@ -63,41 +63,41 @@ public class HoughTransform {
 		long now= System.currentTimeMillis();
 		System.out.println("Hought took: "+(now-start));
 		short max = 0;
-		for(int t=0;t<=twidth;++t)
-			for(int r=0;r<=rmax;++r) {
-				if(max < space_0[t][r])
-					max = space_0[t][r];
-				if(max < space_90[t][r])
-					max = space_90[t][r];
+		for (int angle = 0; angle <= angleSpace; ++angle)
+			for (int dist = 0; dist <= distMax; ++dist) {
+				if (max < space_0[angle][dist])
+					max = space_0[angle][dist];
+				if (max < space_90[angle][dist])
+					max = space_90[angle][dist];
 			}
-		writeHoughImage("space0.png", twidth, rmax, space_0, max);
-		writeHoughImage("space90.png", twidth, rmax, space_90, max);
-		for(int t=0;t<=twidth;++t)
-			for(int r=0;r<=rmax;++r) {
-				if(space_0[t][r] >= max*0.3)
-					coord.add(new double[]{tv_0[t],r});
-				if(space_90[t][r] >= max*THRESHOLD) 
-					coord.add(new double[]{tv_90[t],r});
+		writeHoughImage("space0.png", angleSpace, distMax, space_0, max);
+		writeHoughImage("space90.png", angleSpace, distMax, space_90, max);
+		for (int angle = 0; angle <= angleSpace; ++angle)
+			for (int dist = 0; dist <= distMax; ++dist) {
+				if (space_0[angle][dist] >= max * 0.3)
+					coord.add(new double[] { angle_0[angle], dist });
+				if (space_90[angle][dist] >= max * THRESHOLD)
+					coord.add(new double[] { angle_90[angle], dist });
 			}
 		return null;		
 	}
 
-	private void writeHoughImage(String file, int twidth, int rmax, short[][] space,
+	private void writeHoughImage(String file, int angleSpace, int distMax, short[][] space,
 			short max) {
-		BufferedImage dest = new BufferedImage(twidth+1, rmax+1, BufferedImage.TYPE_INT_BGR);
-		for(int t=0;t<=twidth;++t)
-			for(int r=0;r<=rmax;++r) {
-				int value = 0xff * space[t][r]/max;
-				value = (value<<16)|(value<<8)|value;
-				dest.setRGB(t, r, value);
+		BufferedImage dest = new BufferedImage(angleSpace+1, distMax+1, BufferedImage.TYPE_INT_BGR);
+		for (int a = 0; a <= angleSpace; ++a)
+			for (int d = 0; d <= distMax; ++d) {
+				int value = 0xff * space[a][d] / max;
+				value = (value << 16) | (value << 8) | value;
+				dest.setRGB(a, d, value);
 			}
 		Graphics g = dest.getGraphics();
 		g.setColor(Color.RED);
 		int R = 5;
-		for (int t = 0; t <= twidth; ++t) {
-			for (int r = 0; r <= rmax; ++r) {
-				if (space[t][r] >= max * THRESHOLD)
-					g.drawOval(t - R, r - R, 2 * R, 2 * R);
+		for (int a = 0; a <= angleSpace; ++a) {
+			for (int d = 0; d <= distMax; ++d) {
+				if (space[a][d] >= max * THRESHOLD)
+					g.drawOval(a - R, d - R, 2 * R, 2 * R);
 			}
 		}
 		try {
@@ -107,10 +107,10 @@ public class HoughTransform {
 			e.printStackTrace();
 		}
 	}
-	private void incrementSpace(int rmax, int i, int r, short[][] space) {
-		if (R_MIN <= r && r <= rmax) {
-			if(space[i][r] != MAX_SPACE_VALUE)
-				space[i][r]++;
+	private void incrementSpace(int distMax, int i, int dist, short[][] space) {
+		if (DIST_MIN <= dist && dist <= distMax) {
+			if(space[i][dist] != MAX_SPACE_VALUE)
+				space[i][dist]++;
 		}
 	}
 	private boolean isPoint(BufferedImage src, int x, int y) {
