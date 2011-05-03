25cd643981d62de9ae4aae0f197443b147143e3e
rzymek <rzymek@bc6c1078-3195-aa7d-df54-58ac2b8c4f42>
2011-05-03 20:23:43 +0000

    meterPerPixelY
    git-svn-id: http://mapgrid.googlecode.com/svn/trunk@96 bc6c1078-3195-aa7d-df54-58ac2b8c4f42

--- src/pl/mapgrid/app/actions/UTMGridAction.java
+++ src/pl/mapgrid/app/actions/UTMGridAction.java
@@ -25,31 +25,37 @@ public class UTMGridAction extends AbstractAction implements UIAction {
 		BufferedImage image = main.view.getImage();
 		List<int[]> vertical = new ArrayList<int[]>();
 		List<int[]> horizontal = new ArrayList<int[]>();
-		double n = Math.abs(utm[1].getNorthing() - utm[0].getNorthing());
-		double e = Math.abs(utm[1].getEasting() - utm[0].getEasting());
-		double rot = Math.atan(n/e);
-		double meters = Math.sqrt(n*n+e*e);
-		double metersPerPixel = meters / image.getWidth();
+		double nx = Math.abs(utm[1].getNorthing() - utm[0].getNorthing());
+		double ex = Math.abs(utm[1].getEasting() - utm[0].getEasting());
+		double ny = Math.abs(utm[3].getNorthing() - utm[0].getNorthing());
+		double ey = Math.abs(utm[3].getEasting() - utm[0].getEasting());
+		double rotX = Math.atan(nx/ex);
+		double rotY = Math.atan(ey/ny);
+		double metersX = Math.sqrt(nx*nx+ex*ex);
+		double metersY = Math.sqrt(ny*ny+ey*ey);
+		double metersPerPixelX = metersX / image.getWidth();
+		double metersPerPixelY = metersY / image.getHeight();
 		double start1 = 1000.0 - getStart(utm[0].getEasting());
 		double start2 = Math.abs(utm[0].getEasting() + start1 - utm[3].getEasting());
-		double conv = Math.cos(rot)*metersPerPixel;
-		double x1 = start1/conv;//115
-		double x2 = start2/conv;//141
+		double convX = Math.cos(rotX)*metersPerPixelX;
+		double convY = Math.cos(rotY)*metersPerPixelY;
+		double x1 = start1/convX;//115
+		double x2 = start2/convX;//141
 		for(int i=1000;;i+=1000) {
 			vertical.add(new int[] { (int) x1, 0, (int) x2, image.getHeight() });
-			x1 = (start1+i)/conv;
-			x2 = (start2+i)/conv;
+			x1 = (start1+i)/convX;
+			x2 = (start2+i)/convX;
 			if(x1 > image.getWidth() && x2 > image.getWidth())
 				break;
 		}
 		start1 = getStart(utm[0].getNorthing());
 		start2 = Math.abs(utm[0].getNorthing() - start1 - utm[1].getNorthing());
-		double y1 = start1/conv;
-		double y2 = start2/conv;
+		double y1 = start1/convY;
+		double y2 = start2/convY;
 		for(int i=1000;;i+=1000) {
 			horizontal.add(new int[] { 0, (int) y1, image.getWidth(), (int) y2 });
-			y1 = (start1+i)/conv;
-			y2 = (start2+i)/conv;
+			y1 = (start1+i)/convY;
+			y2 = (start2+i)/convY;
 			if(y1 > image.getHeight() && y2 > image.getHeight())
 				break;
 		}
